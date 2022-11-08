1074a3eec69e8578ba567040aa50a95f66258d5c
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-11-08 09:51:57 +0300

    added initial entity and repository interfaces;

---
+++ src/main/java/com/github/arhor/examples/restrep/data/model/BaseEntity.java
@@ -0,0 +1,8 @@
+package com.github.arhor.examples.restrep.data.model;
+
+public interface BaseEntity<ID> {
+
+    ID getId();
+
+    void setId(ID id);
+}
---
+++ src/main/java/com/github/arhor/examples/restrep/data/repository/BaseRepository.java
@@ -0,0 +1,25 @@
+package com.github.arhor.examples.restrep.data.repository;
+
+import com.github.arhor.examples.restrep.data.model.BaseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseEntity<K>, K> {
+
+    Optional<T> findById(K id);
+
+    List<T> findAllByIds(Iterable<K> ids);
+
+    List<T> findAll();
+
+    T create(T entity);
+
+    T update(K id, T entity);
+
+    void deleteById(K id);
+
+    default void delete(T entity) {
+        deleteById(entity.getId());
+    }
+}
